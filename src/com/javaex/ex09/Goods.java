a05ee4fc468dc30dc926d7c6f710fb38dc8400f9
SeoJongO <eve7884dbqls@naver.com>
2021-06-04 11:20:47 +0900

    6/4 2교시

--- src/com/javaex/ex09/Goods.java
+++ src/com/javaex/ex09/Goods.java
@@ -1,36 +1,52 @@
 package com.javaex.ex09;
 
 public class Goods {
-	//필드
+	// 필드
 	private String name;
 	private int price;
-	private int count;
-	//생성자
+	private static int count;
+
+	// 생성자
 	public Goods() {
 		count = count + 1;
 	}
-	public Goods (String name, int price) {
+
+	public Goods(String name, int price) {
 		super();
 		this.name = name;
 		this.price = price;
 		count = count + 1;
 	}
-	//게터세터
+
+	// 게터세터
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getPrice() {
 		return price;
 	}
+
 	public void setPrice(int price) {
-		this.price = price;
+		int sale = 0;
+		if (price > 0) {
+			this.price = price;
+		} else {
+			this.price = 0;
+			sale = 100;
+		}
+
+		System.out.println(sale);
+
 	}
-	//일반
+
+	// 일반
 	public void showInfo() {
-		System.out.println("상품이름: " + name + "\n가격: " + price+"\n카운트: "+ count+"\n");
+		System.out.println("상품이름: " + name + "\n가격: " + price + "\n카운트: " + count + "\n");
 	}
-	
+
 }
--- src/com/javaex/ex09/GoodsApp.java
+++ src/com/javaex/ex09/GoodsApp.java
@@ -3,20 +3,20 @@ package com.javaex.ex09;
 public class GoodsApp {
 
 	public static void main(String[] args) {
-		
+
 		Goods computer = new Goods("그램", 1000000);
-		
+		computer.showInfo();
+
 		Goods camera = new Goods();
 		camera.setName("니콘");
 		camera.setPrice(400000);
-		
+		camera.showInfo();
+
 		Goods cup = new Goods();
 		cup.setName("머그컵");
-		cup.setPrice(2000 );
-		
-		computer.showInfo();
-		camera.showInfo();
+		cup.setPrice(2000);
 		cup.showInfo();
+
 	}
 
 }
